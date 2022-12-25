e2c5ba4a47ffe8733c870c583c9db8e60f744f0c
shivasurya1999 <64098184+shivasurya1999@users.noreply.github.com>
2022-12-25 18:44:22 -0500

    Update BinarySearchTree.cpp
    Included delete functionality

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -5,6 +5,7 @@ C++ code for Binary search tree
 1. Create BST
 2. Traverse BST inorder 
 3. Search an element in BST, print its location or insert if the node does not exist in the BST 
+4. Delete a node from BST 
 */
 
 #include<iostream>
@@ -26,6 +27,8 @@ public:
 	void Inorder(Node* p);
 	Node* getRoot() { return root; };
 	void SearchAndInsert(Node* p, int element);
+	void Delete(Node* p, int element);
+	Node* FindInOrderPOrS(Node* p);
 };
 
 void BST::CreateBST(Node* p, int next_node_value) { //Function that creates BST from a given root node and a set of node values input by the user 
@@ -104,7 +107,75 @@ void BST::SearchAndInsert(Node* p,int element) { //Function to search and insert
 		if (element < t->rchild->data) t->rchild->lchild = new_elem_node; //if element value is less than the value of the node 't' points to, add the element to the left of the node 't' points to
 		else t->rchild->rchild = new_elem_node; //else add the element to the right of the node 't' points to  
 	}
+}
 
+Node* BST::FindInOrderPOrS(Node* p) { //returns the inorder successor if existing in the BST else the inorder predecessor. If left and right child of p are null, then returns NULL
+	Node* t;
+	t = new Node;
+	t->data = 0;
+	t->lchild = NULL;
+	t->rchild = p;
+	if (p->rchild) { //to find inorder successor go to the right child of the node and then keep going left until null 
+		t->rchild = p->rchild;
+		while (t->rchild->lchild) {
+			t->rchild = t->rchild->lchild;
+		}
+		return t->rchild;
+	}
+	else if (p->lchild) { //to find inorder predecessor go to the left child of the node and then keep going right until null
+		t->rchild = p->lchild;
+		while (t->rchild->rchild) {
+			t->rchild = t->rchild->rchild;
+		}
+		return t->rchild;
+	}
+	else return NULL; //if no inorder predecessor or successor, return NULL 
+}
+
+void BST::Delete(Node *p,int element) {
+	//create a pointer that points to the node 'p'
+	Node* n;
+	n = new Node;
+	n->data = 0;
+	n->lchild = NULL;
+	n->rchild = p;
+	//create a null pointer that follows the pointer 'n'
+	Node* t;
+	t = new Node;
+	t->data = 0;
+	t->lchild = NULL;
+	t->rchild = NULL;
+	while (n->rchild) { //keep looping until 'n' points to null 
+		if (n->rchild->data > element) { //if the node value to which n points to is greater than the new_elem_node value 
+			t->rchild = n->rchild; //update the tail pointer to the node that 'n' is pointing to 
+			n->rchild = n->rchild->lchild; //update 'n' to point to the left child of the current node it points to 
+		}
+		else if (n->rchild->data < element) { //if the node value to which n points to is less than the new_elem_node value
+			t->rchild = n->rchild; // update the tail pointer to the node that 'n' is pointing to
+			n->rchild = n->rchild->rchild; //update 'n' to point to the right child of the current node it points to 
+		}
+		else{
+			//get inorder successor or predecessor 
+			Node* replace_node;
+			replace_node = new Node;
+			replace_node = FindInOrderPOrS(n->rchild);
+			if (replace_node) { //if found
+				int elem = replace_node->data; //copy data into an int variable 
+				Delete(n->rchild,replace_node->data); //Delete that node 
+				n->rchild->data = elem; //copy the data of the replace_node deleted previoudly into the n->rchild 
+				return;
+			}
+			else { //if no successor or predecessor then we have to remove that node from the BST 
+				if (t->rchild) {
+					if (n->rchild == t->rchild->lchild) t->rchild->lchild = NULL; //if the right child of 'n' is the left child of t->rchild
+					else t->rchild->rchild = NULL;
+				}
+				else cout <<endl <<"Requirement was to delete the only node in the BST. Cannot do that. " << endl; //If BST has only one node, we cannot delete it 
+				return;
+			}
+		}
+	}
+	if (t->rchild) cout <<endl<<"Element not found, therefore cannot delete it from the tree. " << endl;
 }
 
 int main() {
@@ -130,4 +201,12 @@ int main() {
 	cout << endl;
 	cout << endl << "Inorder traversal of the BST after inserting element: ";
 	t.Inorder(t.getRoot()); //print BST inorder after performing search and insert 
+	//Node* inOrder;
+	//inOrder = new Node;
+	//inOrder = t.FindInOrderPOrS(t.getRoot());
+	//cout << endl;
+	//cout << "The inorder predecessor or successor is: " << inOrder->data << endl;
+	t.Delete(t.getRoot(),30);
+	cout << endl << "Inorder traversal of the BST after deleting element: ";
+	t.Inorder(t.getRoot());
 }
